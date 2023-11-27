458280a2e139b80c8b5578243498e4adc726fef1
Garrett York <gbyork18@gmail.com>
2023-11-27 06:01:31 -0600

    Basic Character Movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,19 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var speed = 150
+@export var jump_force = 200
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("Right"):
-		animated_sprite.play("Run")
-		
+func _physics_process(delta):
+	if is_on_floor()==false:
+		velocity.y += gravity * delta
+	if velocity.y > 500:
+		velocity.y = 500
+	if Input.is_action_just_pressed("Up") && is_on_floor():
+		velocity.y = -jump_force
+	
+	var direction = Input.get_axis("Left", "Right")
+
+	velocity.x = direction * speed
+	move_and_slide()
